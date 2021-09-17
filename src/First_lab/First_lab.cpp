0c2501cba2788008fe3281d30057f10e346cb028
Mykola <mykola.khlopyk@gmail.com>
2021-09-17 21:49:27 +0300

    add class unfair_dice with cons. and destr.

--- src/First_lab/First_lab.cpp
+++ src/First_lab/First_lab.cpp
@@ -3,6 +3,35 @@
 
 #include <iostream>
 
+#include <cassert>
+
+class Unfair_dice
+{
+public:
+	Unfair_dice(int number_of_sides, double* probability_for_side) {
+		assert(number_of_sides, "Negative number of sides");
+		this->number_of_sides = number_of_sides;
+
+		this->probability_for_side = new double[number_of_sides];
+		double sum_for_checking = 0;//check probabilities input(sum have to be equal to 1)
+		for (int i = 0; i < number_of_sides; i++)
+		{
+			assert(probability_for_side[i], "Negative probability");
+			this->probability_for_side[i] = probability_for_side[i];
+			sum_for_checking += probability_for_side[i];
+		}
+		assert(abs(sum_for_checking - 1) < 0.0001, "Sum of probability doesn't be equal to 1");
+	};
+	~Unfair_dice() {
+		delete[] probability_for_side;
+	};
+
+private:
+	int number_of_sides;
+	double* probability_for_side;
+};
+
+
 int main()
 {
     
