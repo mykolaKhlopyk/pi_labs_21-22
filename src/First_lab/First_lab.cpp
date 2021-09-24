c237124c68fdb41eb6e5fa7c178210eb88e2220f
Mykola <mykola.khlopyk@gmail.com>
2021-09-25 02:58:55 +0300

    added function for finding all combination sum by recursion

--- src/First_lab/First_lab.cpp
+++ src/First_lab/First_lab.cpp
@@ -20,7 +20,7 @@ public:
 		double sum_for_checking = 0;//check probabilities input(sum have to be equal to 1)
 		for (int i = 0; i < number_of_sides; i++)
 		{
-			assert(probability_for_side[i], "Negative probability");
+			assert(probability_for_side[i]>=0, "Negative probability");
 			this->probability_for_side[i] = probability_for_side[i];
 			sum_for_checking += probability_for_side[i];
 		}
@@ -73,11 +73,20 @@ Unfair_dice make_random_dice() {
 	int possible_numbers[] = { 2,4,6,8,10,12,20 };
 	int n = possible_numbers[rand() % 7];
 	double* probabilities = new double[n];
-	probabilities[n - 1] = 1.0;
-	for (int i = 0; i < n - 1; i++)
+	double sum = 0;
+	for (int i = 0; i < n; i++)
 	{
-		probabilities[i] = (rand() % 100) / (n * 100.0);
-		probabilities[n - 1] -= probabilities[i];
+		probabilities[i] = (rand() % 1001) ;
+		sum += probabilities[i];
+	}
+	if (sum==0)
+	{
+		probabilities[0] = 1;
+		sum = 1;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		probabilities[i] = probabilities[i] / sum;
 	}
 	Unfair_dice new_dice(n, probabilities);
 	delete[]probabilities;
@@ -85,13 +94,63 @@ Unfair_dice make_random_dice() {
 	return new_dice;
 }
 
+void find_all_combination_sum_recursion(std::map<int, double>& result, std::vector <Unfair_dice>& dices, int sum = 0, double properties = 1, int current_index = 0) {
+	if (current_index == dices.size())
+	{
+		if (properties != 0)
+		{
+			if (result.count(sum))
+			{
+				result[sum] += properties;
+			}
+			else {
+				result.insert(std::make_pair(sum, properties));
+			}
+		}
+		return;
+	}
+	else {
+
+		for (int i = 0; i < dices[current_index].get_number(); i++)
+		{
+			if (dices[current_index].get_probability_for_side()[i] == 0) {
+				continue;
+			}
+			find_all_combination_sum_recursion(result, dices, sum + (i + 1), properties * (dices[current_index].get_probability_for_side()[i]), current_index + 1);
+
+		}
+	}
+}
+std::map <int, double> find_all_combination_sum(std::vector <Unfair_dice> &dices, bool print=0) {
+	std::map <int, double> result;
+	find_all_combination_sum_recursion(result, dices);
+	if (print)
+	{
+		std::map <int, double> ::iterator it = result.begin();
+		for (int i = 0; it != result.end(); it++, i++) {  // выводим их
+			std::cout << i << ") sum " << it->first << ", probability " << it->second << std::endl;
+		}
+	}
+	return result;
+
+}
+
+
+
 
 int main()
 {
 	srand(time(NULL));
 	Unfair_dice a = make_random_dice();
 	Unfair_dice b = make_random_dice();
+	//Unfair_dice c = make_random_dice();
+	//Unfair_dice d = make_random_dice();
 
 	a.print();
 	b.print();
+	//c.print();
+	//d.print();
+
+	std::vector<Unfair_dice> array = { a,b };
+	find_all_combination_sum(array, 1);
 }
