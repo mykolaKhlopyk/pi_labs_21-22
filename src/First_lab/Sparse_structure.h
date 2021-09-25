18fb3c8ba63856c86bacc690f7b88464a1b60cad
Mykola <mykola.khlopyk@gmail.com>
2021-09-26 02:01:41 +0300

    added functions add() and print() to Sparse_list

--- src/First_lab/Sparse_structure.h
+++ src/First_lab/Sparse_structure.h
@@ -34,6 +34,53 @@ public:
 			delete node_for_deleting;
 		}
 	}
+	void add(T data, int index) {
+		assert(index < this->n, "Incorrect element's index");
+		Node<T>* current = this->root;
+		if (!this->root)
+		{
+			this->root = new Node<T>(data, index);
+		}
+		else if (index < current->pos) {
+			this->root = new Node<T>(data, index);
+			this->root->next = current;
+		}
+		else {
+			while (current->next) {
+				if (current->next->pos == index)
+				{
+					current->next->data = data;
+					return;
+				}
+				else if (current->next->pos > index) {
+					Node<T>* new_node = new Node<T>(data, index);
+					new_node->next = current->next;
+					current->next = new_node;
+					return;
+				}
+				current = current->next;
+			}
+			current->next = new  Node<T>(data, index);
+		}
+	}
+
+	void print() {
+		Node<T> *current = this->root;
+		for (int i = 0; i < this->n; i++)
+		{
+			if (current)
+			{
+				if (current->pos == i)
+				{
+					std::cout << current->data << "\t";
+					current = current->next;
+					continue;
+				}
+			}
+			std::cout << 0 << "\t";
+		}
+		std::cout << "\n";
+	}
 private:
 	Node<T>* root;
 	int n;
