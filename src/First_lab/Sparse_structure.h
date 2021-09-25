6cc382e18edb94389e638d3112c85de8c053ef9b
Mykola <mykola.khlopyk@gmail.com>
2021-09-26 01:33:27 +0300

    created class Sparse_list with constructors and destructors

--- src/First_lab/Sparse_structure.h
+++ src/First_lab/Sparse_structure.h
@@ -14,3 +14,27 @@ public:
 		this->next = nullptr;
 	}
 };
+
+template<typename T>
+class Sparse_list
+{
+public:
+	Sparse_list() {
+		this->root = nullptr;
+		this->n = 0;
+	}
+	Sparse_list(int n) :Sparse_list() {
+		this->n = n;
+	}
+	~Sparse_list() {
+		Node<T>* current = this->root;
+		while (current) {
+			Node<T>* node_for_deleting = current;
+			current = current->next;
+			delete node_for_deleting;
+		}
+	}
+private:
+	Node<T>* root;
+	int n;
+};
